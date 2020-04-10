046f0a5c25c831b40e048f31132a54d88ac8a1ca
rrochlin <43162265+rrochlin@users.noreply.github.com>
2020-04-10 15:20:17 -0700

    Working version using the Auto time length data.

--- notebooks/Users/roberr23@uw.edu/Analytics.py
+++ notebooks/Users/roberr23@uw.edu/Analytics.py
@@ -7,15 +7,87 @@ import glob
 # COMMAND ----------
 
 #gathering csv data using glob
-all_csv_files = glob.glob("/dbfs/FileStore/tables/2019/*.csv")
+all_csv_files = glob.glob("/dbfs/FileStore/tables/Auto/*.csv")
 print("--" + str(all_csv_files))
 loops=len(all_csv_files)
 all_csv_files
 
 # COMMAND ----------
 
-pd.read_csv("/dbfs/FileStore/tables/RTU_1_BldgStatPress_2019-343cc.csv")
+#TS2Sec function def. converts datestamps from the crown point building to seconds from jan 2019.
+def monthstime(month):
+        if month =='Jan':  
+            return 0
+        elif month == 'Feb':  
+            return 31
+                     
+        elif month ==  'Mar':  
+            return 31+28
+                   
+        elif month == 'Apr':  
+            return 31+28+31
+                     
+        elif month == 'May':  
+            return 31+28+31+30
+                     
+        elif month == 'Jun':  
+            return 31+28+31+30+31
+                     
+        elif month == 'Jul':  
+            return 31+28+31+30+31+30
+                     
+        elif month == 'Aug':  
+            return 31+28+31+30+31+30+31
+                     
+        elif month == 'Sep':  
+            return 31+28+31+30+31+30+31+31
+                     
+        elif month == 'Oct': 
+            return 31+28+31+30+31+30+31+31+30
+                     
+        elif month == 'Nov': 
+            return 31+28+31+30+31+30+31+31+30+31
+                     
+        elif month == 'Dec': 
+            return 31+28+31+30+31+30+31+31+30+31+30
+                     
+        
 
+
+
+def TS2Sec(x):
+    loops=len(x)
+    for i in range(loops):
+        total=0
+        month=x.iat[i,0]
+        #days month is dd-MMM-YY HH:mm:ss am or pm
+        temp=month.split('-')[0]
+        month=month.split('-')[1]+'-'+month.split('-')[2]
+        total+=int(temp)*24*60**2
+        #months month is MMM-YY HH:mm:ss am or pm
+        temp=month.split('-')[0]
+        month=month.split('-')[1]
+        total+=monthstime(temp)*24*60**2
+        #year month is YY HH:mm:ss am or pm
+        temp=month.split(' ')[0]
+        month=month.split(' ')[1]+' '+month.split(' ')[2]
+        total+=(int(temp)-18)*365*24*60**2
+        #hours month is HH:mm:ss am or pm
+        temp=month.split(':')[0]
+        month=month.split(':')[1]+':'+month.split(':')[2]
+        total+=int(temp)*60**2
+        #minutes month is mm:ss am or pm
+        temp=month.split(':')[0]
+        month=month.split(':')[1]
+        total+=int(temp)*60
+        #seconds month is ss am or pm
+        temp=month.split(' ')[0]
+        month=month.split(' ')[1]
+        total+=int(temp)*60
+        #am/pm month is am or pm
+        if month == 'pm':
+            total+=12*60**2
+        x.iat[i,0]=total
 
 # COMMAND ----------
 
@@ -23,87 +95,30 @@ import numpy as np
 varlist=[]
 
 for i in range(int(loops)):
-    globals() ['df'+str(i)] = pd.read_csv(all_csv_files[i])
-    globals() ['df'+str(i)].set_index('Timestamp', inplace=True)
-    if i == 0:
-      result = df0
+    if i == int(loops-1):
+      globals() ['df'+str(i)] = pd.read_csv(all_csv_files[0])
     else:
-      result = result.merge(globals() ['df'+str(i)], on='Timestamp',how='left')
+      globals() ['df'+str(i)] = pd.read_csv(all_csv_files[int(i+1)])
+    if i == 0:
+      Result = df0
+    else:
+      Result = Result.merge(globals() ['df'+str(i)], on='Timestamp',how='left')
       
-result
-
-# COMMAND ----------
-
-
-import pandas as pd
-import numpy as np
- 
-
-# generating some test data
-timestamp = [1440540000, 1450540000]
-df1 = pd.DataFrame(
-    {'timestamp': timestamp, 'a': ['val_a', 'val2_a'], 'b': ['val_b', 'val2_b'], 'c': ['val_c', 'val2_c']})
-print(df1)
- 
-
-# building a different index
-timestamp = timestamp * np.random.randn(abs(1))
-df2 = pd.DataFrame(
-    {'timestamp': timestamp, 'd': ['val_d', 'val2_d'], 'e': ['val_e', 'val2_e'], 'f': ['val_f', 'val2_f'],
-     'g': ['val_g', 'val2_g']}, index=timestamp)
-print(df2)
- 
-
-# keeping a value in common with the first index
-timestamp = [1440540000, 1450560000]
-df3 = pd.DataFrame({'timestamp': timestamp, 'h': ['val_h', 'val2_h'], 'i': ['val_i', 'val2_i']}, index=timestamp)
-print(df3)
-
- 
-# Setting the timestamp as the index
-df1.set_index('timestamp', inplace=True)
-df2.set_index('timestamp', inplace=True)
-df3.set_index('timestamp', inplace=True)
- 
-# You can convert timestamps to dates but it's not mandatory I think
-df1.index = pd.to_datetime(df1.index, unit='s')
-df2.index = pd.to_datetime(df2.index, unit='s')
-df3.index = pd.to_datetime(df3.index, unit='s')
- 
-# Just perform a join and that's it
-#result=df1.merge(df2, on='timestamp',how='left').merge(df3, on='timestamp',how='left')
-result = df1.join(df2, how='outer').join(df3, how='outer')
-result
+Result
 
 # COMMAND ----------
 
 ax = plt.gca()
+Result.plot(kind='line',x='Timestamp',y='RTU_1 OutdoorTemp(°F)',color='blue',ax=ax)
+Result.plot(kind='line',x='Timestamp',y='RTU_1 RATemp(°F)', color='red', ax=ax)
+plt.xticks(rotation='vertical')
 
-result.plot(kind='line',x='Timestamp',y='RTU_1 OutdoorTemp(°F)',ax=ax)
-#RATemp2019.plot(kind='line',x='Timestamp',y='RTU_1 RATemp(°F)', color='red', ax=ax)
-#plt.xticks(rotation='vertical')
-
-plt.show()
 #here we're looking at retrun air vs outdoor temp
 #anywhere where OutDoorTemp>RATemp we expect the 
 #economizer to be active
+display(ax)
+
 
 # COMMAND ----------
 
-result['Timestamp']
-
-# COMMAND ----------
-
-print(all_csv_files)
-
-# COMMAND ----------
-
-dbfs ls
-
-# COMMAND ----------
-
-f
-
-# COMMAND ----------
-
-#this line is changed
+|
